5c65eca7d666c58f7c38ce3ec4d13d49bc987d76
rizkidy <rizkidy99@gmail.com>
2023-05-29 07:47:16 +0700

    change API_KEY

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: 'd5392ad4b382b074a64084f63a7bfbc2',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
